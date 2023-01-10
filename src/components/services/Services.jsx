9363a063c80d3b60079847069d0f460edb031f58
mrinal975 <mrinalmallik1@gmail.com>
2023-01-11 01:46:46 +0600

    portfolio service portion

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,68 @@
 import React from "react";
 import "./services.css";
-
+import { BiCheck } from "react-icons/bi";
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container service__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Deb Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of WebDevelopment */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Operational</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Publishing and used to demonstrate the visual form</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
