6b12fb6b92fe6f70cc2708775bec277d60c7bd11
mrinal975 <mrinalmallik1@gmail.com>
2023-01-10 23:07:10 +0600

    about page design with media uery

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 import "./about.css";
-
+import ME from "../../assets/horror.jpg";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>4+ years Working Experience</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Experience</h5>
+              <small>4+ years Working Experience</small>
+            </article>
+          </div>
+          <p>
+            In publishing and graphic design, Lorem ipsum is a placeholder
+            text commonly used to demonstrate the visual form of a document
+            or a typeface without relying on meaningful content. Lorem
+            ipsum may be used as a placeholder.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
-
 
 export default About;
