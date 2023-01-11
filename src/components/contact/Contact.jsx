90c9af8dc3fcaad668c6ac440326ae93aa953192
mrinal975 <mrinalmallik1@gmail.com>
2023-01-11 22:31:52 +0600

    media query rename and contact form design

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,71 @@
 import React from "react";
 import "./contact.css";
-import {MdOutlineEmail} from 'react-icons/md';
-import {RiMessengerFill} from 'react-icons/ri';
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return (<section id="contact">
-    <h5>Get In Touch</h5>
-    <h2>Contact Me</h2>
-    <div className="container contact_container">
-      <article className="contact_option">
-        <MdOutlineEmail />
-        <h4>Email</h4>
-        <h5>dumyMail@gmail.com</h5>
-        <a href="dumyMail@gmail.com">Send a Message</a>
-      </article>
-      <article className="contact_option">
-        <RiMessengerFill />
-        <h4>Message</h4>
-        <h5>dumyMail@gmail.com</h5>
-        <a href="dumyMail@gmail.com">Send a Message</a>
-      </article>
-      <article className="contact_option">
-        <RiMessengerFill />
-        <h4>What's app</h4>
-        <h5>dumyMail@gmail.com</h5>
-        <a href="dumyMail@gmail.com">Send a Message</a>
-      </article>
-    </div>
-    {/* END OF CONTACT OPTION */}
-    <form action="">
-
-    </form>
-  </section>);
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className="contact_option-icon" />
+            <h4>Email</h4>
+            <h5>dumyMail@gmail.com</h5>
+            <a href="mailto:mrinal.mallik.se@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine className="contact_option-icon" />
+            <h4>Message</h4>
+            <h5>dumyMail@gmail.com</h5>
+            <a href="https://m.me/mrinalmallik80051/" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp className="contact_option-icon" />
+            <h4>What's app</h4>
+            <h5>+8801632319339</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+8801632319339"
+              target="_blank"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTION */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            required
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
