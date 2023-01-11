e9e726fcd1abb5f56a4f39873a7cf6ffd1c44cff
mrinal975 <mrinalmallik1@gmail.com>
2023-01-11 23:47:49 +0600

    footer page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./footer.css";
-
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        EGATOR
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a href="https://facebook.com/">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com/">
+          <FiInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy;Mrinal Mallik. All right reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
